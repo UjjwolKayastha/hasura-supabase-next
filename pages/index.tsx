de7a3aad115b29d44aacfcd57f80acaf88d2e809
UjjwolKayastha <uzol123@gmail.com>
2022-06-29 18:22:48 +0545

    feat: image implmentation (design)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Auth, Typography, Button } from '@supabase/ui';
+import { Auth, Typography, Button, Image } from '@supabase/ui';
 import { supabase } from '../utils/supabaseClient';
 
 const Home = (props: any) => {
@@ -6,7 +6,25 @@ const Home = (props: any) => {
   if (user)
     return (
       <div style={{ padding: 30 }}>
-        <Typography.Text>Signed in: {user.email}</Typography.Text>
+        <div
+          style={{
+            display: 'flex',
+            width: '100%',
+            alignItems: 'center',
+            marginBottom: 15,
+            flexDirection: 'column',
+            gap: 15,
+          }}
+        >
+          <Image
+            source={`${process.env.NEXT_PUBLIC_SUPABASE_BUCKET_URL}/${user?.user_metadata.profile_image}`}
+            alt="profile"
+          />
+          <input name="profile_image" type="file" />
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <Typography.Text>Signed in: {user.email}</Typography.Text>
+        </div>
         <Button block onClick={() => props.supabaseClient.auth.signOut()}>
           Sign out
         </Button>
